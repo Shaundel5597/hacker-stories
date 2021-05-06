8c7086742922cbe136fc134edbe76d2fed48cc03
Shaundel5597 <naunie08@yahoo.com>
2021-05-06 08:37:12 -0400

    added search bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+const title = "React";
 
 function App() {
-  const title = "React"
   return (
     <div>
-      <h1> hello World </h1>
+      <h1> hello {title} </h1>
+    
+    <label htmlFor="search"> Search: </label>
+    <input id="search" type="text" />
     </div>
   );
 }
