11cdc67a0875e62e2dc06026db183c7fea22f561
Shaundel5597 <naunie08@yahoo.com>
2021-05-06 08:45:20 -0400

    added a new list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
 
-function getTitle(title) {
-  return title;
-}
+const list = [
+  {
+  title: 'React',
+  url: 'https://reactjs.org/',
+  author: 'Jordan Walke',
+  num_comments: 3,
+  points: 4,
+  objectID: 0,
+  },
+  {
+  title: 'Redux',
+  url: 'https://redux.js.org/',
+  author: 'Dan Abramov, Andrew Clark',
+  num_comments: 2,
+  points: 5,
+  objectID: 1,
+  }
+];
 
 function App() {
   return (
